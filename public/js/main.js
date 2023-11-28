99304cea7e74b6621ed8ddd06daec64c4ba7ea34
VasKho <vasya.khoroshavin@gmail.com>
2023-11-28 19:03:33 +0300

    feat: add popups for opening documents

--- public/js/main.js
+++ public/js/main.js
@@ -7,6 +7,22 @@ const body_load = () => {
       search(search_field.value);
     }
   };
+
+  document.querySelector("#help").onclick = () => {
+    const text = 'To start search just type your query in the search bar and press "Enter"\n To specify the word that must be in the document wrap it with double quotes ("")';
+    create_popup(text);
+  };
+
+  document.querySelector("#metrics").onclick = () => {
+    let text = '',
+	recall = "recall: 0.75391\n",
+	precision = "precision: 0.76815\n",
+	accuracy = "accuracy: 0.83012\n",
+	error = "error: 0.29147\n",
+	f_measure = "f-measure: 0.73934\n";
+    text += recall + precision + accuracy + error + f_measure;
+    create_popup(text);
+  };
 };
 
 const open_url = (url) => {
@@ -47,6 +63,24 @@ const add_search_result = (name, words, url) => {
 
 const open_file = async (url) => {
   const response = await fetch(url, {
-    mode: "same-origin"
+    mode: "same-origin",
+    headers: { "Content-Type": "text/plain" }
   }).then(response=>response.blob());
+  create_popup(await response.text());
+};
+
+const create_popup = async (content) => {
+  const popup_box = document.createElement("div");
+  popup_box.className = "popup";
+  const close_button = document.createElement("button");
+  close_button.className = "close-button";
+  close_button.textContent = "close";
+  close_button.onclick = () => {
+    popup_box.parentNode.removeChild(popup_box);
+  };
+  popup_box.appendChild(close_button);
+  const text_box = document.createElement("div");
+  text_box.innerText = content;
+  popup_box.appendChild(text_box);
+  document.body.appendChild(popup_box);
 };
